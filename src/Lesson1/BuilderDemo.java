070a5f1b20648d958bd17b8fb069158cfa06cbed
Waldemar91 <volodymyr.shenbor@gmail.com>
2024-01-05 09:04:28 +0200

    Lesson1 - lecture + resolve

---
+++ src/Lesson1/BuilderDemo.java
@@ -0,0 +1,20 @@
+package Lesson1;
+
+public class BuilderDemo {
+    public static void main(String[] args) {
+        Product product = Product.builder()
+                .name("Ноутбук")
+                .price(10_000)
+                .description("опис")
+                .build();
+
+        Product customProduct = Product.builderCustom()
+                .name("Ноутбук")
+                .price(10_000)
+                .description("опис")
+                .build();
+
+        System.out.println(product);
+        System.out.println(customProduct);
+    }
+}
---
+++ src/resolveshw1/Car.java
@@ -0,0 +1,21 @@
+package resolveshw1;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+//@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Car {
+
+
+    private String model;
+    private int power;
+    private int volume;
+
+    @ToString.Exclude
+    private boolean turbo;
+}
